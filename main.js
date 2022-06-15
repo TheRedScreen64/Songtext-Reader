79511a63747abeada12632c8fe33d0b013341eb1
TheRedScreen <joni.schaefer@magenta.de>
2022-06-15 18:13:48 +0200

    Update 1.1

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { app, BrowserWindow, autoUpdater, dialog  } = require('electron');
+const { app, BrowserWindow, autoUpdater, ipcMain  } = require('electron');
 
 let mainWindow;
+let settingsWindow;
 
 //Functions
 function createWindow () {
@@ -19,7 +20,7 @@ function createWindow () {
   
     mainWindow.loadFile('index.html')
     mainWindow.removeMenu()
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
 
     require('update-electron-app')({
         repo: 'TheRedScreen64/Songtext-Reader',
@@ -29,6 +30,23 @@ function createWindow () {
     mainWindow.on('close', () => {
         app.quit()
     })
+}
+
+function createSettingsWindow(){
+    settingsWindow = new BrowserWindow({
+        icon: __dirname + '/res/icon.ico',
+        // frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        width: 400,
+        height: 600
+    })
+  
+    settingsWindow.loadFile('settings.html')
+    settingsWindow.removeMenu()
+    settingsWindow.webContents.openDevTools()
 }
 
 app.on('ready', createWindow);
@@ -43,4 +61,14 @@ app.on('activate', function() {
     if (mainWindow === null) {
       createWindow();
     }
+});
+
+ipcMain.on('settings:open',function(e){
+    createSettingsWindow()
+});
+
+ipcMain.on('settings',function(e, data){
+    console.log(data)
+
+    mainWindow.webContents.send('settings', data)
 });
